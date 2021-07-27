7a15f5b792882b3458bb8cb571e627779ba92a5a
admin <py601826@gmail.com>
2021-07-27 17:07:18 +0530

    search bar

--- lib/Ui/NotificationScreen.dart
+++ lib/Ui/NotificationScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:kontribute/Common/fab_bottom_app_bar.dart';
 import 'package:kontribute/Ui/AddScreen.dart';
 import 'package:kontribute/Ui/HomeScreen.dart';
 import 'package:kontribute/Ui/SettingScreen.dart';
